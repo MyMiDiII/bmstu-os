2bcfc8c60cd4b41ed4d8f65933fef600ff6a0e35
MyMiDiII <mymidi.student@yandex.ru>
2021-10-26 16:17:28 +0300

    ADD: load GDT and int heandlers

--- sem01/lab02/src/main.asm
+++ sem01/lab02/src/main.asm
@@ -36,7 +36,7 @@ data32 segment para 'DATA'
     gdt_size = $ - gdt_null
 
 ; псевдодескриптор (6 байт)
-    psdesct df 0
+    psdescr df 0
    
 ; селекторы
     data4gbs = 8
@@ -47,15 +47,15 @@ data32 segment para 'DATA'
 
 ; таблица дескрипторов прерываний
     idt label byte
-    ;idesct0_12   idesct 13 dup (<0,code32s,0,8Fh,0>)
-    ;idesct13     idesct <0,code32s,0,8Fh,0>
-    ;idesct14_31  idesct 18 dup (<0,code32s,0,8Fh,0>)
-    ;int_time     idesct <0,code32s,0,Eh,0>
-    ;int_keyboard idesct <0,code32s,0,Eh,0>
+    idescr0_12      idescr 13 dup (<dummy,code32s,0,8Fh,0>)
+    idescr13        idescr <except13,code32s,0,8Fh,0>
+    idescr14_31     idescr 18 dup (<dummy,code32s,0,8Fh,0>)
+    idescr_time     idescr <0,code32s,0,Eh,0>
+    idescr_keyboard idescr <0,code32s,0,Eh,0>
     idt_size = $ - idt
 
 ; псевдодекскриптор (6 байт) 
-    ipsdesct df 0
+    ipsdescr df 0
 
 ; сообщения
     rm_msg     db 27,'[32;20mREAL MODE',27, '[0m', 13, 10, '$'
@@ -67,6 +67,55 @@ data32 segment para 'DATA'
 
     data32_size = $ - gdt_null 
 data32 ends
+
+code32 segment para public 'CODE' use32
+    assume cs:code32, ds:data32, ss:stack32
+
+pm_begin:
+; заглушка для исключений
+    dummy proc
+        iret
+    dummy endp
+
+; заглушка для 13-ого исключения 
+; при возникновении этого исключения в стек помещается
+; код ошибки, который в обработчике мы снимаем
+    except13 proc uses eax
+        pop eax
+        iret
+    except13 endp
+
+    int8h proc uses eax 
+        mov edi, 80 * 2 + 2 * 30
+        xor eax, eax
+
+        mov ah, color
+        rol ah, 4
+        mov al, ' '
+        stosw		
+
+        cmp cnt_time, 5
+        jne skip
+
+        mov cnt_time, 11111111b
+        inc ah
+        cmp ah, 1111b
+        jne save
+        mov ah, 0000b
+
+    save:
+        mov color, ah
+            
+    skip:	
+        inc cnt_time
+
+        mov	al, 20h 
+        out	20h, al
+        iretd
+    int8h endp
+
+    code32_size = $ - pm_begin
+code32 ends
 
 code16 segment para public 'CODE' use16
     assume cs:code16, ds:data32, ss:stack32
@@ -89,6 +138,50 @@ begin:
 ; ожидание нажатия клавиши
     mov ax, 10h
     int 16h
+
+; очистка экрана
+    mov ax, 3h
+    int 10h
+
+; подготовка к переходу в защищенный режим
+; вычисление и загрузка в GDT линейных адресов сегментов
+    xor eax, eax
+    mov ax, code32
+    shl eax, 4
+    mov word ptr gdt_code32.base_l, ax
+    shr eax, 16
+    mov byte ptr gdt_code32.base_m, al
+    mov byte ptr gdt_code32.base_h, ah
+    
+    xor eax, eax
+    mov ax, data32
+    shl eax, 4
+    mov word ptr gdt_data32.base_l, ax
+    shr eax, 16
+    mov byte ptr gdt_data32.base_m, al
+    mov byte ptr gdt_data32.base_h, ah
+
+    xor eax, eax
+    mov ax, stack32
+    shl eax, 4
+    mov word ptr gdt_stack32.base_l, ax
+    shr eax, 16
+    mov byte ptr gdt_stack32.base_m, al
+    mov byte ptr gdt_stack32.base_h, ah
+
+; подготовка псевдодескриптора и загрузка GDTR
+    mov ax, data32
+    shl eax, 4
+    add eax, offset gdt_null
+    mov dword ptr psdescr + 2, eax
+    mov word  ptr psdescr, gdt_size - 1
+    lgdt fword prt psdescr
+
+; переход в защищенный режим
+
+; возращение в реальный режим
+
+; работа в реальном режиме
 
 ; вывод сообщения о возвращении в реальный режим
     mov ah, 09h
