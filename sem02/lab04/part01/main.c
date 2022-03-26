35d880212bb77db6d4df17664321534baa1056db
MyMiDiII <mymidi.student@yandex.ru>
2022-03-26 13:44:26 +0300

    ADD: pagemap

--- sem02/lab04/part01/main.c
+++ sem02/lab04/part01/main.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 #include "stat_expl.h"
 
@@ -207,28 +210,77 @@ void get_comm_info(const char *proc, FILE *out)
     fclose(f);
 }
 
-//void get_pagemap_info(const char *proc, FILE *out)
-//{
-//    int m_fd = open("/proc/self/maps", O_RDONLY);
-//    int pm_fd = open("/proc/self/pagemap", O_RDONLY);
-//
-//    uint64_t start_address = strtoul(argv[2], NULL, 0);
-//    uint64_t end_address = strtoul(argv[3], NULL, 0);
-//
-//    for(uint64_t i = start_address; i < end_address; i += 0x1000) {
-//        uint64_t data;
-//        uint64_t index = (i / PAGE_SIZE) * sizeof(data);
-//        if(pread(fd, &data, sizeof(data), index) != sizeof(data)) {
-//            perror("pread");
-//            break;
-//        }
-//
-//        print_page(i, data);
-//    }
-//
-//    close(fd);
-//    return 0;
-//}
+void print_page(uint64_t address, uint64_t data, FILE *out)
+{
+    fprintf(out, "0x%-16lx : %-16lx %-10ld %-10ld %-10ld %-10ld\n",
+        address,
+        data & (((uint64_t)1 << 55) - 1),
+        (data >> 55) & 1,
+        (data >> 61) & 1,
+        (data >> 62) & 1,
+        (data >> 63) & 1);
+}
+
+void get_pagemap_info(const char *proc, FILE *out)
+{
+    fprintf(out, "PAGEMAP\n");
+    fprintf(out, "       addr        : pfn          "
+                 "soft-dirty file/shared "
+                 "swapped    present\n");
+    char path[PATH_MAX];
+    snprintf(path, PATH_MAX, "/proc/%s/maps", proc);
+
+    FILE *maps = fopen(path, "r");
+
+    snprintf(path, PATH_MAX, "/proc/%s/pagemap", proc);
+    int pm_fd = open(path, O_RDONLY);
+
+    char buf[BUF_SIZE + 1] = "\0";
+    int len;
+
+    while ((len = fread(buf, 1, BUF_SIZE, maps)) > 0)
+    {
+        for (int i = 0; i < len; ++i)
+            if (buf[i] == 0)
+                buf[i] = '\n';
+
+        buf[len] = '\0';
+
+        char *save_row;
+        char *row = strtok_r(buf, "\n", &save_row);
+
+        while (row)
+        {
+            char *addresses = strtok(row, " ");
+
+            char *start_str, *end_str;
+
+            if ((start_str = strtok(addresses, "-")) &&
+                (end_str = strtok(NULL, "-")))
+            {
+
+                uint64_t start = strtoul(start_str, NULL, 16);
+                uint64_t end = strtoul(end_str, NULL, 16);
+
+                for(uint64_t i = start; i < end; i += sysconf(_SC_PAGE_SIZE))
+                {
+                    uint64_t offset;
+                    uint64_t index = ((i / sysconf(_SC_PAGE_SIZE))
+                                         * sizeof(offset));
+
+                    pread(pm_fd, &offset, sizeof(offset), index);
+                    print_page(i, offset, out);
+                }
+            }
+
+            row = strtok_r(NULL, "\n", &save_row);
+        }
+
+    }
+
+    fclose(maps);
+    close(pm_fd);
+}
 
 char *get_proc_id(int argc, char **argv)
 {
@@ -242,8 +294,6 @@ char *get_proc_id(int argc, char **argv)
     {
         snprintf(id, ID_SIZE, "%s", argv[1]);
     }
-
-    printf("%s\n", id);
 
     return id;
 }
@@ -264,7 +314,7 @@ int main(int argc, char **argv)
     get_stat_info(id, out);
     get_io_info(id, out);
     get_maps_info(id, out);
-    //get_pagemap_info(id, out);
+    get_pagemap_info(id, out);
 
     fclose(out);
 
