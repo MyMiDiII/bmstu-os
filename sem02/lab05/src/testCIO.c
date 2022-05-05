61068b808b94ea87763d23abf5487f8ae1c7e842
MyMiDiII <mymidi.student@yandex.ru>
2022-05-05 19:59:57 +0300

    ADD: lab605 program 1 full

---
+++ sem02/lab05/src/testCIO.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define GREEN "\033[01;38;05;46m"
+#define BLUE  "\033[01;38;05;33m"
+#define CLEAR "\033[0m"
+
+int main(void)
+{
+    fprintf(stdout, "Program 1.\n");
+    int fd = open("alphabet.txt", O_RDONLY);
+
+    FILE *fs1 = fdopen(fd, "r");
+    char buff1[20];
+    setvbuf(fs1, buff1, _IOFBF, 20);
+
+    FILE *fs2 = fdopen(fd, "r");
+    char buff2[20];
+    setvbuf(fs2, buff2, _IOFBF, 20);
+
+    int flag1 = 1, flag2 = 1;
+
+    while(flag1 == 1 || flag2 == 1)
+    {
+        char c;
+
+        flag1 = fscanf(fs1, "%c", &c);
+        if (flag1 == 1)
+            fprintf(stdout, GREEN "%c" CLEAR, c);
+
+        flag2 = fscanf(fs2, "%c", &c);
+        if (flag2 == 1) 
+            fprintf(stdout, BLUE "%c" CLEAR, c); 
+
+    }
+
+    fprintf(stdout, "\n");
+    return 0;
+}
